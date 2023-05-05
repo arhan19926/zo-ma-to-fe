202ea7bfc24ef466b2340ab8054297484f472818
arhan19926 <ali.arhan19926@gmail.com>
2023-05-05 11:14:42 +0530

    fixed some minor css issues

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     
     <form className={styles.loginForm} action="#" onSubmit={()=>console.log(`submitted`)}>
-      <input type="text" className={styles.textField} placeholder='Username' />
+      <input type="text" className={styles.textField} placeholder='Email' />
       <input type="password" className={styles.textField} placeholder='Password' />
       <button type="submit" className={styles.submitButton}>Login</button>
     </form>
