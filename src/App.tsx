11f25563e09ced68ac8b7d90892a0858774ba6c1
arhan19926 <ali.arhan19926@gmail.com>
2023-05-09 17:14:19 +0530

    fixed some css issues with the carousel library

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
-import styles from "./sass/app.module.scss";
-import Login from "./pages/login/rightPanelLogin";
-import HomePage from "./pages/login/leftPanel";
-import Navbar from "./components/navbar";
-import Maincontainer from "./pages/login/mainContainer";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
